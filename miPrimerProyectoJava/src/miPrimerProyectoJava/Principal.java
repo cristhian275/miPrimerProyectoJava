7f85b316c30d538c2655faad4474bf942c7edcec
crist <crist@DESKTOP-FPGPNST>
2021-02-10 10:01:54 -0300

    Alterações nos comentarios

--- miPrimerProyectoJava/src/miPrimerProyectoJava/Principal.java
+++ miPrimerProyectoJava/src/miPrimerProyectoJava/Principal.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 /**
  * @author crist
  *DAta 03/02/2021
- */
+ *//
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Olá amigo!\nQual é o seu nome?");
